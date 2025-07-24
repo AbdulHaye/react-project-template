96ed21a809934c149e5d8c995967d82bf46376a0
zainulhassan360 <zainulhassan@360synergytech.com>
2025-07-24 22:25:46 +0500

    Add 404 page.

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import MovieDetails from "@/app/pages/movie-details";
 import Movies from "@/app/pages/movies";
+import NotFound from "@/app/pages/not-found";
 import Search from "@/app/pages/search";
 import { Layout } from "@/components/layout/layout";
 
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Movies />} />
         <Route path="/search" element={<Search />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
